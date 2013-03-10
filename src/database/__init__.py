3a924ebac8ecd1c8ff1dcbf60b9e5ea45fa58554
Jan Losinski <losinski@wh2.tu-dresden.de>
2013-03-10 18:53:46 +0100

    Add documentation on the database session stuff.

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -3,10 +3,33 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.pool import StaticPool
 
 
+# The global database session.
 session = None
 
 
 def init_session(connection_string=None, drop=False):
+    """Initialize the database session and create the schema if it
+    does not exist.
+
+    The connection string describes the database connection.
+    Documentation on this can be found on [1]. If its omitted
+    a temporary in-memory sqlite database will be used. This
+    is useful for unittesting where you need a fresh database
+    on every run.
+
+    The schema can also be dropped before initialization by
+    setting the drop parameter to true.
+
+    The session can be accessed by the session variable of the
+    database module.
+
+    [1] http://docs.sqlalchemy.org/en/rel_0_8/core/engines.html
+
+    :param connection_string: The connection description for the
+                              engine. See above for details
+    :param drop: Drop the schema and recreate it in init. All
+                 data will be lost!
+    """
     if connection_string is None:
         engine =  create_engine('sqlite://',
                                 echo=True,
