0fd2676efbc6214fba7d2c82e2b19b0b8c28a23d
Jan Losinski <losinski@wh2.tu-dresden.de>
2013-03-26 22:53:20 +0100

    Reformat

--- src/webapp/dummy_data.py
+++ src/webapp/dummy_data.py
@@ -12,12 +12,12 @@ def make_dummy_data(num_teams, confirmed=True):
 
         for member_idx in range(3):
             member = Members(name="Member%d from team%d" % (member_idx, idx),
-                             team = team)
+                             team=team)
             db.session.add(member)
 
         lat_rand = (0.5 - random()) * 0.1
         lon_rand = (0.5 - random()) * 0.1
-        pseudo_dist = sqrt(lat_rand ** 2 + lon_rand **2)
+        pseudo_dist = sqrt(lat_rand ** 2 + lon_rand ** 2)
         lat = 51.0322627 + lat_rand
         lon = 13.7071665 + lon_rand
 
