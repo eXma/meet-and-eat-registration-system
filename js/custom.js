bad0b2214e8fd2c1da3db70734674fd8bdfbda96
mcnesium <mcnesium@versteht.es>
2013-03-28 19:02:43 +0100

    codestyle "->'

--- js/custom.js
+++ js/custom.js
@@ -32,9 +32,9 @@
 			var form;
 			switch (data.step) {
 				case 1:
-					if (data.direction == "next") {
-						form = $("#step1").find(">form");
-						if (form.find("input").jqBootstrapValidation("hasErrors")) {
+					if (data.direction == 'next') {
+						form = $('#step1').find('>form');
+						if (form.find('input').jqBootstrapValidation('hasErrors')) {
 							form.submit();
 							e.preventDefault();
 							return;
@@ -42,10 +42,10 @@
 					}
 					break;
 				case 2:
-					if (data.direction != "next")
+					if (data.direction != 'next')
 						break;
 					if (map !== undefined || searcher !== undefined || searcher.getSelectedLocation() !== undefined) {
-						console.log("Your location is: " + searcher.getSelectedLocation());
+						console.log('Your location is: ' + searcher.getSelectedLocation());
 					} else {
 						e.preventDefault();
 						return;
@@ -53,27 +53,27 @@
 					break;
 				case 3:
 					if (data.direction === 'next') {
-						form = $("#step3").find(">form");
-						if (form.find("input").jqBootstrapValidation("hasErrors")) {
+						form = $('#step3').find('>form');
+						if (form.find('input').jqBootstrapValidation('hasErrors')) {
 							form.submit();
 							e.preventDefault();
 							return;
 						}
 						var resultData = {
-							teamname: $("#inputName").val(),
-							email: $("#inputEmail").val(),
-							phone: $("#inputPhone").val(),
-							street: $("#inputStreet").val(),
-							zip: $("#inputZip").val(),
+							teamname: $('#inputName').val(),
+							email: $('#inputEmail').val(),
+							phone: $('#inputPhone').val(),
+							street: $('#inputStreet').val(),
+							zip: $('#inputZip').val(),
 							geolocation: searcher.getSelectedLocation(),
-							member1: $("#inputMemberName1").val(),
-							member2: $("#inputMemberName2").val(),
-							member3: $("#inputMemberName3").val(),
-							allergies: $("#allergies").val(),
-							vegetarians: $("#vegetarians").val(),
-							accepted: $("#terms").is(":checked")
+							member1: $('#inputMemberName1').val(),
+							member2: $('#inputMemberName2').val(),
+							member3: $('#inputMemberName3').val(),
+							allergies: $('#allergies').val(),
+							vegetarians: $('#vegetarians').val(),
+							accepted: $('#terms').is(':checked')
 						};
-						alert("(Would) send data via ajax:\n" + JSON.stringify(resultData, null, '\t'));
+						alert('(Would) send data via ajax:\n' + JSON.stringify(resultData, null, '\t'));
 						haveNext = false;
 					}
 
@@ -84,35 +84,35 @@
 					return;
 
 			}
-			myNextBtn.attr("disabled", false === haveNext);
-			myPrevBtn.attr("disabled", false === havePrev);
+			myNextBtn.attr('disabled', false === haveNext);
+			myPrevBtn.attr('disabled', false === havePrev);
 		});
 		myWizard.on('changed', function (e, data) {
-			var item = myWizard.data("wizard").selectedItem().step;
+			var item = myWizard.data('wizard').selectedItem().step;
 			if (item === 2) {
 				if (map == undefined) {
 					displayMap();
 				}
-				var qry = $("#inputStreet").val() + ", " + $("#inputZip").val() + ", Dresden";
+				var qry = $('#inputStreet').val() + ', ' + $('#inputZip').val() + ', Dresden';
 				searcher.geosearch(qry);
-				$modal.find(".btn-primary").on("click", function (e) {
+				$modal.find('.btn-primary').on('click', function (e) {
 					e.preventDefault();
 					console.log(searcher.getSelectedLocation());
 					alert(searcher.getSelectedLocation());
 				});
 			}
-			console.log('changed');
+			console.log('changed',item);
 
 		});
 		myWizard.on('finished', function (e, data) {
 			console.log('finished');
-			myWizard.data("wizard").$prevBtn.attr("disabled", true);
+			myWizard.data('wizard').$prevBtn.attr('disabled', true);
 		});
 
 		myPrevBtn.on('click', function () {
 			$('#myWizard').wizard('previous');
 		});
-		myPrevBtn.attr("disabled", true);
+		myPrevBtn.attr('disabled', true);
 		myNextBtn.on('click', function () {
 			$('#myWizard').wizard('next', 'foo');
 		});
@@ -120,5 +120,5 @@
 			var item = $('#myWizard').wizard('selectedItem');
 			console.log(item.step);
 		});
-		$("input").jqBootstrapValidation();
+		$('input').jqBootstrapValidation();
 	});
