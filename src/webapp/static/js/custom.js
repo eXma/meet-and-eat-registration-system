b4df0db6bed5c70fde377408251fcdc8c9177667
mcnesium <mcnesium@versteht.es>
2013-04-05 13:48:42 +0200

    remove browserupdate (didnt work)

--- src/webapp/static/js/custom.js
+++ src/webapp/static/js/custom.js
@@ -165,17 +165,6 @@ $(function () {
 	$('input').jqBootstrapValidation();
 });
 
-// Browser-Update Hinweis
-var $buoop = {vs:{i:8,f:4,o:11,s:4,n:9}};
-$buoop.ol = window.onload; 
-window.onload=function(){ 
-try {if ($buoop.ol) $buoop.ol();}catch (e) {} 
-	var e = document.createElement("script"); 
-	e.setAttribute("type", "text/javascript"); 
-	e.setAttribute("src", "http://browser-update.org/update.js"); 
-	document.body.appendChild(e); 
-};
-
 $('#del-vegetarian').on('click', function(e){
 	e.preventDefault();
 	$('button[name="vegetarian"].active').removeClass('active');
