7efa40ccfdd602e0b78827e2270e174874be2a2d
Jan Losinski <losinski@wh2.tu-dresden.de>
2013-04-11 00:31:08 +0200

    Too much Java today ...

--- src/geotools/__init__.py
+++ src/geotools/__init__.py
@@ -1,7 +1,7 @@
 from geotools.routing import Router
 
 
-def simpleDistance(point_a, point_b):
+def simple_distance(point_a, point_b):
     with Router() as router:
         route = router.route(point_a, point_b)
         return route.distance
