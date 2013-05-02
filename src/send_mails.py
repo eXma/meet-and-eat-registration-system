885a4914f6fdd6290719cef094440fab11e5362f
Jan Losinski <losinski@wh2.tu-dresden.de>
2013-05-02 02:55:45 +0200

    Implement sending of evening plans.

--- src/send_mails.py
+++ src/send_mails.py
@@ -1,7 +1,8 @@
 #!env python
 
 from argparse import ArgumentParser
-from teammails import informal_to_teams
+from plan_tool import read_plan_file
+from teammails import informal_to_teams, plans_to_teams
 
 
 def parse_args():
@@ -13,14 +14,24 @@ def parse_args():
     informal = subs.add_parser("informal", help="send informal mails")
     informal.add_argument("-s", "--subject", help="set the mail subject", required=True)
     informal.add_argument("-t", "--templatename", help="set the name of the template to use", required=True)
-
     informal.set_defaults(func=cmd_send_informal)
+
+    plan = subs.add_parser("plan", help="send the plan mails")
+    plan.add_argument("--inform", required=True, choices=("legacy", "dan_marc_partial"),
+                      help="select the input format for the plan")
+    plan.add_argument("in_file", help="The input file with the plan")
+    plan.set_defaults(func=cmd_send_planmails)
 
     return args.parse_args()
 
 
 def cmd_send_informal(args):
     informal_to_teams(args.templatename, args.subject, not args.nodebug)
+
+
+def cmd_send_planmails(args):
+    results = read_plan_file(args)
+    plans_to_teams(results, not args.nodebug)
 
 
 if __name__ == "__main__":
--- src/teammails/__init__.py
+++ src/teammails/__init__.py
@@ -1,4 +1,6 @@
+from collections import defaultdict
 from contextlib import contextmanager
+from email.header import make_header
 import os
 import smtplib
 import database as db
@@ -56,6 +58,94 @@ def informal_to_teams(template_name, subject, debug=True):
             msg['To'] = rcpt
 
             session.sendmail(envelope, [rcpt] + ["redaktion@exmatrikulationsamt.de"], msg.as_string())
-            i+=1
+            i += 1
         print "Mails sent: %d" % i
 
+
+def plans_to_teams(plan_results, debug=True):
+    guestentry = """
+%(time)s Uhr - %(roundname)s bei "%(host)s"
+%(address)s
+Klingeln bei: %(bell)s
+Bei Problemen erreicht ihr dieses Team unter: %(phone)s
+Routenlink: %(link)s"""
+
+    hostentry = """
+%(time)s Uhr - %(roundname)s wird von Euch zubereitet. (%(teamname)s)
+
+zu Gast sind bei Euch:
+%(guests)s"""
+
+    hostguests = """
+  Team "%(guestname)s"
+  (Allergien: %(allergies)s)
+  (Vegeratier dabei: %(vegetarians)s)
+  Telefon: %(guestphone)s
+"""
+    subject = "Meet&Eat - Abendplanung Team %s"
+    round_datas = ({"time": "18:00", "name": "Vorspeise"},
+                   {"time": "19:30", "name": "Hauptgericht"},
+                   {"time": "21:00", "name": "Dessert"})
+
+    print "Preprocess plan..."
+    guestmap = defaultdict(list)
+    for team in plan_results:
+        for host in plan_results[team]:
+            if team != host:
+                guestmap[host].append(team)
+
+    template = get_template("plan")
+    sender = "meet&eat Orga <%s>" % config.MAIL_DEFAULT_SENDER
+    envelope = config.MAIL_DEFAULT_SENDER
+
+    print "Fetch data..."
+    teams = {}
+    for team in db.session.query(Team).filter_by(deleted=False).filter_by(confirmed=True):
+        teams[str(team.id)] = team
+
+    i = 0
+    print "Connect to smtp..."
+    with smtp_session() as session:
+        print "Send mails..."
+        for team in plan_results:
+            plan_detail = []
+            for (round_idx, host) in enumerate(plan_results[team]):
+                round_data = round_datas[round_idx]
+                if team != host:
+                    detail = guestentry % {"time": round_data["time"],
+                                           "roundname": round_data["name"],
+                                           "host": teams[host].name,
+                                           "address": teams[host].location.street,
+                                           "bell": teams[host].location.extra,
+                                           "phone": teams[host].phone,
+                                           "link": "WURSt"}
+                    plan_detail.append(detail)
+                else:
+                    guest_details = []
+                    for guest in guestmap[team]:
+                        guest_detail = hostguests % {"guestname": teams[guest].name,
+                                                     "allergies": teams[guest].allergies,
+                                                     "vegetarians": teams[guest].vegetarians,
+                                                     "guestphone": teams[guest].phone}
+                        guest_details.append(guest_detail)
+                    detail = hostentry % {"time": round_data["time"],
+                                          "roundname": round_data["name"],
+                                          "teamname": teams[team].name,
+                                          "guests": "\n".join(guest_details)}
+                    plan_detail.append(detail)
+            plan = "\n\n".join(plan_detail)
+            text = template.render(eventdate=config.EVENT_DATE, teamname=teams[team].name, plan=plan)
+
+            msg = MIMEText(text, "plain", "utf8")
+
+            rcpt = teams[team].email
+            if debug:
+                rcpt = config.MAIL_DEFAULT_SENDER
+
+            msg['Subject'] = subject % teams[team].name
+            msg['From'] = sender
+            msg['To'] = rcpt
+
+            session.sendmail(envelope, [rcpt] + ["redaktion@exmatrikulationsamt.de"], msg.as_string())
+            i += 1
+        print "Mails sent: %d" % i
