00b688d529e69db2f32bd78416aea37f3727a553
Jan Losinski <losinski@wh2.tu-dresden.de>
2013-12-07 02:15:38 +0100

    Fix typos in location fetch
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

--- src/location_fetch.py
+++ src/location_fetch.py
@@ -19,16 +19,18 @@ for (idx, team_from) in enumerate(teams):
     location_from = MapPoint.from_team(team_from)
     for team_to in teams[(idx + 1):]:
         route_a = db.session.query(RouteDistance).filter_by(
-            location_from=team_from.location, location_to=team_to.location).fist()
+            location_from=team_from.location, location_to=team_to.location).first()
         route_b = db.session.query(RouteDistance).filter_by(
-            location_to=team_from.location, location_from=team_to.location).fist()
+            location_to=team_from.location, location_from=team_to.location).first()
 
-        if route_a is not None and route_b is not None:
+        if (route_a is not None) and (route_b is not None):
             continue
 
         location_to = MapPoint.from_team(team_to)
 
         dist = int(simple_distance(location_from, location_to) * 1000)
+        log = u"%s -> %s :: %d" % (team_from.name, team_to.name, dist)
+        print log.encode("utf8")
 
         if route_a is None:
             distances.append(RouteDistance(location_from=team_from.location, location_to=team_to.location, distance=dist))
