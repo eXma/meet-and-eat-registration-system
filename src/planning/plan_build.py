b900d4882b4bd06149c9278aac129b78fc63a50b
Jan Losinski <losinski@wh2.tu-dresden.de>
2015-11-06 23:19:23 +0100

    Refactor round data reading

--- src/planning/plan_build.py
+++ src/planning/plan_build.py
@@ -1,10 +1,12 @@
 import imp
 import json
 from collections import defaultdict
-from math import floor
+
+from sqlalchemy import not_
+
 import database as db
 from database.model import Team, MeetingEntry
-from sqlalchemy import not_
+from planning.rounds import round_data
 
 
 def read_legacy_plan(in_file):
@@ -17,65 +19,87 @@ def read_legacy_plan(in_file):
     return result
 
 
-def read_dan_marc_partial(in_file, group=None, seperate=None, exclude=None):
-    """Debug output prefixed with "data="
+def read_dan_marc_partial(in_file, group=None, separate=None, exclude=None):
+    """Read Debug output prefixed with "data="
 
+    :param exclude: List of teams to exclude from processing
+    :param group: A group id to process
+    :param separate: A list of teams to include
     :param in_file: The file with the result
     :return: The processed data
     """
     contents = imp.load_source("_dummy", in_file)
+    return process_dan_marc_partial(contents.data,
+                                    fetch_teams(group, separate, exclude))
 
+
+def fetch_teams(group=None, separate=None, exclude=None):
+    """Fetch a selection of teams
+
+    :param exclude: List of teams to exclude from processing
+    :type exclude: list[int] or None
+    :param group: A group id to process
+    :type group: int or None
+    :param separate: A list of teams to include
+    :type separate: list[int] or None
+    :return: The selected teams as list
+    :rtype: list[Team]
+    """
     teams = db.session.query(Team).filter_by(deleted=False,
                                              confirmed=True,
                                              backup=False).order_by(Team.id)
-    if seperate is not None:
-        teams = teams.filter(Team.id.in_(seperate))
+    if separate is not None:
+        teams = teams.filter(Team.id.in_(separate))
     if exclude is not None:
         teams = teams.filter(not_(Team.id.in_(exclude)))
     if group:
         teams = teams.filter_by(groups=group)
 
-    teams = teams.all()
-    round_teams = defaultdict(list)
+    return teams.all()
 
-    max_working = len(teams) - (len(teams) % 3)
-    divider = max_working / 3.0
 
-    def distance_sort(a, b):
-        if a.location.center_distance > b.location.center_distance:
-            return -1
-        if a.location.center_distance < b.location.center_distance:
-            return 1
-        return 0
+def process_dan_marc_partial(contents, team_data):
+    """process Debug output prefixed with "data="
 
-    working = teams[:max_working]
-    teams = sorted(working, distance_sort) + teams[max_working:]
+    :param team_data: A list of selected teams
+    :type team_data: list[Team]
+    :param contents: The parsed contents
+    :return: The processed data
+    """
 
-    for idx, team in enumerate(teams):
-        round_idx = 0
-        if (divider > 0):
-            round_idx = min(int(floor(idx / divider)), 3)
-        round_teams[round_idx].append(idx)
+    round_teams = {}
+    team_list = []
 
-    plans_idx = defaultdict(list)
-    for (round_idx, round_entry) in enumerate(contents.data):
+    for (team, round_idx) in round_data(team_data):
+        team_list.append(team)
+        round_teams[team.id] = round_idx
+
+    def iter_meeting(meeting_set):
+        for idx in meeting_set:
+            yield team_list[idx].id
+
+    plans = defaultdict(list)
+    for (round_idx, round_entry) in enumerate(contents):
         for meeting in round_entry:
             host = None
-            for team_idx in meeting:
-                if team_idx in round_teams[round_idx]:
-                    host = team_idx
-                    break
-            for team_idx in meeting:
-                plans_idx[team_idx].append(host)
+            participants = []
+            for team_id in iter_meeting(meeting):
+                participants.append(team_id)
+                if round_teams[team_id] == round_idx:
+                    host = team_id
 
-    def map_team(idx):
-        return str(teams[idx].id)
+            for team_id in participants:
+                plans[team_id].append(host)
 
+    return string_indexes(plans)
+
+
+def string_indexes(team_data):
     result = defaultdict(list)
-    for team_idx in plans_idx:
-        team_id = map_team(team_idx)
-        for station in plans_idx[team_idx]:
-            result[team_id].append(map_team(station))
+
+    for team in team_data:
+        for station in team_data[team]:
+            result[str(team)].append(str(station))
 
     return result
 
@@ -83,21 +107,21 @@ def read_dan_marc_partial(in_file, group=None, seperate=None, exclude=None):
 def read_database_plan():
     plans = defaultdict(list)
     for entry in db.session.query(MeetingEntry).order_by(MeetingEntry.plan_round):
-        plans[str(entry.participant)].append(str(entry.host))
+        plans[entry.participant].append(entry.host)
 
-    return plans
+    return string_indexes(plans)
 
 
 def build_meeting_entries(plans):
     meeting_parts = set()
     for entry in plans:
-        round = 0
+        round_idx = 0
         for station in plans[entry]:
             part = MeetingEntry(host_team_id=int(station),
-                                plan_round=round,
+                                plan_round=round_idx,
                                 participant_team_id=int(entry))
             meeting_parts.add(part)
-            round += 1
+            round_idx += 1
 
     return meeting_parts
 
--- test/test_planning.py
+++ test/test_planning.py
@@ -1,6 +1,7 @@
 import unittest
-
-from planning.plan_build import build_meeting_entries
+import database as db
+from mock import Mock
+from planning.plan_build import build_meeting_entries, process_dan_marc_partial
 
 
 class TestMeetingEntryBuild(unittest.TestCase):
@@ -17,3 +18,87 @@ class TestMeetingEntryBuild(unittest.TestCase):
 
         entries = build_meeting_entries(plan)
         self.assertEqual(len(entries), 9 * 3)
+
+
+def _mock_team(idx, distance):
+    team = Mock(id=idx)
+    team.location = Mock(center_distance=distance)
+    return team
+
+
+class TestDanMarcProcessing(unittest.TestCase):
+    def setUp(self):
+        db.init_session(drop=True)
+        self.maxDiff = None
+
+    # noinspection PySetFunctionToLiteral
+    def test_processing(self):
+        data_input = [[set([0, 22, 15]),
+                       set([1, 26, 17]),
+                       set([2, 18, 10]),
+                       set([3, 20, 23]),
+                       set([25, 19, 4]),
+                       set([24, 11, 5]),
+                       set([12, 13, 6]),
+                       set([16, 9, 7]),
+                       set([8, 21, 14])],
+                      [set([9, 4, 6]),
+                       set([24, 25, 10]),
+                       set([19, 18, 11]),
+                       set([23, 12, 7]),
+                       set([3, 5, 13]),
+                       set([2, 26, 14]),
+                       set([8, 1, 15]),
+                       set([16, 20, 22]),
+                       set([0, 17, 21])],
+                      [set([8, 17, 18]),
+                       set([1, 10, 19]),
+                       set([20, 14, 15]),
+                       set([2, 11, 21]),
+                       set([4, 13, 22]),
+                       set([16, 5, 23]),
+                       set([0, 24, 6]),
+                       set([9, 12, 25]),
+                       set([26, 3, 7])]]
+
+        team_ids = [(3, 3.3973), (4, 4.3275), (5, 3.5067), (8, 3.8383),
+                    (12, 3.3185), (13, 3.9332), (14, 4.3404), (16, 2.7504),
+                    (19, 4.1135), (21, 3.3217), (23, 3.3088), (25, 4.4788),
+                    (26, 3.8302), (28, 3.3362), (29, 4.4257), (30, 3.8994),
+                    (31, 2.826), (33, 4.1537), (35, 3.3217), (38, 4.2229),
+                    (39, 4.4579), (45, 3.6162), (46, 5.4541), (49, 3.2621),
+                    (50, 3.9429), (51, 4.4788), (53, 2.9354)]
+
+        expected = {'49': ['39', '30', '49'],
+                    '53': ['14', '50', '53'],
+                    '25': ['25', '45', '21'],
+                    '26': ['33', '26', '35'],
+                    '21': ['29', '13', '21'],
+                    '23': ['46', '5', '23'],
+                    '46': ['46', '3', '53'],
+                    '45': ['46', '45', '35'],
+                    '28': ['51', '13', '28'],
+                    '29': ['29', '19', '23'],
+                    '3': ['25', '3', '28'],
+                    '5': ['38', '5', '49'],
+                    '4': ['4', '19', '53'],
+                    '8': ['4', '8', '23'],
+                    '13': ['14', '13', '12'],
+                    '12': ['33', '3', '12'],
+                    '39': ['39', '8', '16'],
+                    '38': ['38', '30', '16'],
+                    '14': ['14', '8', '49'],
+                    '16': ['25', '26', '16'],
+                    '19': ['38', '19', '31'],
+                    '31': ['29', '50', '31'],
+                    '30': ['4', '30', '31'],
+                    '51': ['51', '26', '12'],
+                    '50': ['51', '50', '21'],
+                    '35': ['39', '5', '35'],
+                    '33': ['33', '45', '28']}
+
+        # noinspection PyTypeChecker
+        result = process_dan_marc_partial(data_input,
+                                          [_mock_team(idx, dist)
+                                           for (idx, dist) in team_ids])
+        self.assertDictEqual(dict(result), expected)
